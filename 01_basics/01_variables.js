0ca2ff9f26bdcd3bda36efb1d24df281a6c0aeed
DeveshKumarGaur <97347314+DeveshKumarGaur@users.noreply.github.com>
2024-06-27 15:42:05 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "devesh@gmail.com"
+var accountPassword = "12345678"
+/*
+prefer not to use var keyword as it has no control on block scope
+*/ 
+accountCity = "Delhi"
+let accountState
+//accountId = 2
+console.log(accountId);
+console.table([accountId, accountEmail,accountCity, accountPassword, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert("Hello") we are using node.js not browser
+
+console.log(3+3)
+console.log("Hello")
+
+let name = "Hitesh" // String
+let age = 18 // number => 2^53
+let isBoolean = false // boolean
+let email = null // null datatype have a standalone value which of type object
+let password // undefined => value is not defined yet
+//symbol is a datatype which is unique
+
+console.log(typeof(age));
+console.log(typeof(null));
